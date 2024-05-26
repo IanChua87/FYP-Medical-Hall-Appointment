697c89d87b81e84e66ffa16c8a340282dd30f561
IanChua87 <22024905@myrp.edu.sg>
2024-05-26 21:59:28 +0800

    CSS file
    Made minor adjustments to CSS

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
             crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="style.css" />
 </head>
 <body>
 
@@ -160,7 +160,7 @@
                         <div class="mb-3">
                             <textarea class="form-control feedback-field" name="message" rows="6" placeholder="Feedback"></textarea>
                         </div>
-                        <button type="submit" class="btn submit-btn">Submit</button>
+                        <a class="btn submit-btn" href="forms/contactSuccessful.php" role="button">Submit</a>
                     </form>
                 </div>
     
