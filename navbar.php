b4c07f4457447aa592bba6d7af24b9aea8331c8e
IanChua87 <22024905@myrp.edu.sg>
2024-07-19 15:39:29 +0800

    commit
    commit

--- navbar.php
+++ navbar.php
@@ -11,23 +11,23 @@ echo '
 
         <div class="collapse navbar-collapse" id="navbarMenu">
             <ul class="navbar-nav ms-auto">'; ?>
-            <?php if (!isset($_SESSION["patient_id"])) { ?>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#services">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#contact">Contact</a>
-                </li>
+<?php if (!isset($_SESSION["patient_id"])) { ?>
+    <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#about">About</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#services">Services</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#contact">Contact</a>
+    </li>
 <?php } else { ?>
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="../P_index.php">Home</a>
-                </li>
+    <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="../P_index.php">Home</a>
+    </li>
 <?php } ?>
 <?php echo '
                 
@@ -37,26 +37,26 @@ echo '
 if (isset($_SESSION['patient_id'])) {
     echo '
         <div class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Appointment
-                </a>
-            <ul class="dropdown-menu" aria-labelledby="apptDropdown">
-                <li><a class="dropdown-item" href="booking.php">Book Appointment</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="viewappointment.php">View Appointment</a></li>
-            </ul>
-        </div>
-        <div class="nav-item dropdown">
-            <a class="nav-linkdropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="bi bi-person-circle"></i>
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="userDropdown">
-                <li><a class="dropdown-item" href="editprofile.php">Edit Profile</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="changepassword.php">Change Password</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="loggedOutSuccessful.php">Logout</a></li>
-            </ul>
+            <a class="nav-link dropdown-toggle" id="apptDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Appointment
+    </a>
+    <ul class="dropdown-menu" aria-labelledby="apptDropdown">
+        <li><a class="dropdown-item" href="booking.php">Book Appointment</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="viewappointment.php">View Appointment</a></li>
+    </ul>
+</div>
+<div class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="bi bi-person-circle"></i>
+    </a>
+    <ul class="dropdown-menu" aria-labelledby="userDropdown">
+        <li><a class="dropdown-item" href="editprofile.php">Edit Profile</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="changepassword.php">Change Password</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="loggedOutSuccessful.php">Logout</a></li>
+    </ul>
         </div>';
 } else {
     echo '<ul class="nav navbar-nav">
