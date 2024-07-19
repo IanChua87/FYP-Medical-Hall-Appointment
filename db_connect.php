475cea25968bfaaa313bc3c743f6a7beebbe9f11
IanChua87 <22024905@myrp.edu.sg>
2024-07-19 17:17:28 +0800

    Update db_connect.php
    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,26 +1,26 @@
 <?php
-$host = "localhost:3304";
-$username = "root";
-$password = "";
-$dbname = "fyp_sin_nam";
+// $host = "localhost:3304";
+// $username = "root";
+// $password = "";
+// $dbname = "fyp_sin_nam";
 
-// Create connection
-$conn = mysqli_connect($host, $username, $password, $dbname);
+// // Create connection
+// $conn = mysqli_connect($host, $username, $password, $dbname);
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+// // Check connection
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
 
 
-// $conn = mysqli_init();
+$conn = mysqli_init();
 
-// mysqli_ssl_set($conn, NULL, NULL, "", NULL, NULL);
+mysqli_ssl_set($conn, NULL, NULL, "", NULL, NULL);
 
-// $host = 'sinnamwebapp.mysql.database.azure.com';
-// $username = 'snwebappadmin';
-// $password = 'qXQ$gCByydOXpY9s';
-// $database = 'fyp_sin_nam';
-// $port = 3306;
+$host = 'sinnammed001server.mysql.database.azure.com';
+$username = 'turkdhuewv';
+$password = 'RYzd9GjQ43$Y56RZ';
+$database = 'fyp_sin_nam';
+$port = 3306;
 
-// mysqli_real_connect($conn, $host, $username, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, $host, $username, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL);
